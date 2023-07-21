aaa7e61911ae6e1a79125ca9538baa9c512f5926
omullen <lavendericana@gmail.com>
2023-07-21 21:21:08 +0300

    commit new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { CurrentWeatherComponent } from './current-weather/current-weather.compo
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ICurrentWeather } from '../icurrent-weather';
 
 @Component({
   selector: 'app-current-weather',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./current-weather.component.css']
 })
 export class CurrentWeatherComponent {
+  current: ICurrentWeather
+  constructor(private weatherService: WeatherService) {
+    this.current = {
+      city: '',
+      country: '',
+      date: new Date(),
+      image: '',
+      temperature: 0,
+      description: '',
+    }
+  }
 
 }
